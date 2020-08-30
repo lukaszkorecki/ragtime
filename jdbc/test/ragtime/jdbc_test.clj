bcd9be8cc6775e32e6355884ff7c606f1694e061
Łukasz Korecki <lukasz@korecki.me>
2020-08-30 15:37:12 -0700

    Revert

--- jdbc/test/ragtime/jdbc_test.clj
+++ jdbc/test/ragtime/jdbc_test.clj
@@ -68,7 +68,7 @@
   (test-sql-migration db-spec { :transactions true }))
 
 (deftest test-sql-migration-using-db-spec-with-existing-connection
-  (jdbc/with-db-connection
+  (sql/with-db-connection
     [conn db-spec]
     (test-sql-migration conn {})))
 
